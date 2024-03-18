7cd8cc9d9de072561c0e7ad86e24d9138ac54122
yaayala <105887914+yaayala@users.noreply.github.com>
2024-03-17 21:08:28 -0500

    :art: new ec2 boot script to the jenkins instance

--- _scm_terraform/ec2.tf
+++ _scm_terraform/ec2.tf
@@ -1,14 +1,24 @@
-resource "aws_instance" "jenkins-ec2" {
+resource "aws_instance" "jenkins-master" {
   ami                    = var.mapping.dv.ami_id
   instance_type          = var.mapping.dv.instance_type
   subnet_id              = aws_subnet.public_subnets["us-east-1a"].id
   vpc_security_group_ids = [aws_security_group.sg-jenkins-ssh-public.id, aws_security_group.sg-jenkins-https-http-public.id]
-  user_data              = file("scripts/boot-script.sh")
+  user_data              = file("scripts/install-jenkins.sh")
   key_name               = aws_key_pair.linux-key-pair.key_name
-  tags                   = merge(var.mapping.dv.general_tags, { Name = "ec2-${local.sufix}" })
+  tags                   = merge(var.mapping.dv.general_tags, { Name = "master-${local.sufix}" })
 }
 
-output "my-public-ip" {
-  value = aws_instance.jenkins-ec2.public_ip
+# resource "aws_instance" "jenkins-agent" {
+#   ami                    = var.mapping.dv.ami_id
+#   instance_type          = var.mapping.dv.instance_type
+#   subnet_id              = aws_subnet.public_subnets["us-east-1a"].id
+#   vpc_security_group_ids = [aws_security_group.sg-jenkins-ssh-public.id, aws_security_group.sg-jenkins-https-http-public.id]
+#   user_data              = file("scripts/boot-script.sh")
+#   key_name               = aws_key_pair.linux-key-pair.key_name
+#   tags                   = merge(var.mapping.dv.general_tags, { Name = "agent-${local.sufix}" })
+# }
+
+output "jenkins-public-ip" {
+  value = aws_instance.jenkins-master.public_ip
   description = "Public IP address of the Jenkins instance"
 }
